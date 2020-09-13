acb6fa601fe6b893d56664980ecd361da3f4ff0d
jeffreycorpuz <jeffreysobienacorpuz@gmail.com>
2020-09-13 22:05:01 +0800

    Created the Contact Us Page and Fix the Layout of the page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,258 @@
+@extends('layouts.master')
+
+@section('title')
+    bfox.gg
+@endsection
+
+@section('page_css')
+    <script src="https://kit.fontawesome.com/cf37de91f8.js" crossorigin="anonymous"></script>  
+
+    <style>
+        #banner{
+            height: 35vh;
+            font-family: 'Alfa Slab One', cursive;
+            justify-content: center;
+            align-items: center;
+            display: flex;
+            text-shadow: -1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white;
+        }
+
+        #banner::after {
+            height: 40vh;
+            content: "";
+            background-image: url('/images/content-1.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            opacity: 0.5;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            position: absolute;
+            z-index: -1;   
+        }
+
+        #banner h1{
+            margin: 0px;
+        }
+
+        #contact-us .row{
+            height: 50vh;
+            color: white;
+            font-family: 'Titillium Web', sans-serif;
+            margin-bottom: 3rem;
+        }
+
+        #reach-us{
+            border-radius: 20px 0px 0px 20px;
+            background-color: #485461;
+            background-image: linear-gradient(315deg, #485461 0%, #28313b 74%);
+        }
+
+        #reach-us h3{
+            margin-bottom: 3rem;
+            width: 145px;
+            border-bottom: 1px solid red;
+        }
+
+        #reach-us a{
+            padding: 0px;
+        }
+
+        #reach-us span{
+            font-size: 18px;
+            color: white;
+        }
+
+        #reach-us i{
+            margin-right: 1rem;
+            font-size: 24px;
+        }
+
+        #reach-us .twitter-link:hover{
+            color: #1DA1F2 !important;
+        }
+
+        #reach-us .facebook-link:hover{
+            color: #3C5A99;
+        }
+
+        #reach-us .instagram-link:hover{
+            color: #B43162;
+        }
+
+        #reach-us .weibo-link:hover{
+            color: #DF2029;
+        }
+
+        #reach-us .gmail-link:hover{
+            color: #3A6742;
+        }
+
+        #email-form{
+            border-radius: 0px 20px 20px 0px;
+            background-color: #485461;
+        }
+
+        #email-form input{
+            padding: 0px;
+            background-color: #485461;
+            border: 0;
+            border-bottom: 2px solid #4a5777;
+            border-radius: 0px;
+            color: white;
+
+        }
+
+        #email-form textarea{
+            padding: 0px;
+            background-color: #485461;
+            border: 0;
+            border-bottom: 2px solid #4a5777 ;
+            border-radius: 0px;
+            color: white;
+            overflow-y: auto;
+
+        }
+
+
+        input[type="text"]::placeholder { /* Firefox, Chrome, Opera */ 
+            color: #757575;
+        } 
+        
+        input[type="text"]:-ms-input-placeholder { /* Internet Explorer 10-11 */ 
+            color: #757575; 
+        } 
+        
+        input[type="text"]::-ms-input-placeholder { /* Microsoft Edge */ 
+            color: #757575; 
+        }
+
+        input[type="email"]::placeholder { /* Firefox, Chrome, Opera */ 
+            color: #757575; 
+        } 
+        
+        input[type="email"]:-ms-input-placeholder { /* Internet Explorer 10-11 */ 
+            color: #757575; 
+        } 
+        
+        input[type="email"]::-ms-input-placeholder { /* Microsoft Edge */ 
+            color: #757575; 
+        } 
+
+        textarea[id="exampleFormControlTextarea1"]::placeholder { /* Firefox, Chrome, Opera */ 
+            color: #757575; 
+        } 
+        
+        textarea[id="exampleFormControlTextarea1"]:-ms-input-placeholder { /* Internet Explorer 10-11 */ 
+            color: #757575; 
+        } 
+        
+        textarea[id="exampleFormControlTextarea1"]::-ms-input-placeholder { /* Microsoft Edge */ 
+            color: #757575; 
+        }
+
+        #button-container{
+            width: 100%;
+            display: flex;
+            justify-content: flex-end;
+            right: 0.5rem;
+            bottom: 1rem;
+            position: absolute;
+        }
+
+        #email-form button{
+            height: 6rem;
+            border-radius: 5rem;
+            border: 1px solid white;
+            color: white;
+            transition: all 1s ease;
+        }
+
+        #email-form button:hover {
+            transform: scale(1.1);
+        }
+
+
+    </style>
+@endsection
+
+@section('nav')
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="#" class="navbar-brand"><img src="images/sample_fox_icon.png" id="logo">BudFox</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Matches</a></li>
+                <li class="nav-item"><a href="/contact" class="nav-link active"><u>Contact</u></a></li>
+            </ul>
+        </div>
+    </nav>
+@endsection
+
+@section('content')
+    <!-- banner -->
+    <div class="mt-3" id="banner">
+        <div class="container">
+            <div class="col-sm text-center">
+                <h1 class="display-4">Contact Us</h1>
+                <p>Feel free to reach out to us.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mb-4" id="contact-us">
+        <div class="row">
+            <div class="col-lg-4 p-5" id="reach-us">
+                <h3 id="title-container" class="pb-3">Reach us at:</h3>
+
+                <a href="https://twitter.com/bfoxesports" class="nav-link twitter-link"><span class="twitter-link"><i class="fab fa-twitter"></i>@bfoxesports</span></a>
+                <br />
+                <a href="https://www.facebook.com/BfoxMobileLegends/" class="nav-link"><span class="facebook-link"><i class="fab fa-facebook-f"></i> /BfoxMobileLegends</span></a>
+                <br />
+                <a href="https://www.instagram.com/bfoxesports" class="nav-link"><span class="instagram-link"><i class="fab fa-instagram"></i>@bfoxesports</span></a>
+                <br />
+                <a href="#" class="nav-link" ><span class="weibo-link"><i class="fab fa-weibo"></i><span class="weibo-link">@BfoxMobileLegends</span></a>
+                <br />
+                <a href="https://mail.google.com/mail/u/0/" class="nav-link"><span class="gmail-link"><i class="fab fa-google"></i>bfox@gmail.com</span></a>
+            </div>
+
+            <div class="col-lg-8 p-5" id="email-form">
+
+                <label class="font-weight-bold">Name </label>
+                <br />
+                <input type="text" name="full_name" class="form-control" placeholder="Name">
+
+                <label class="font-weight-bold">Email </label>
+                <br />
+                <input type="email" name="email" class="form-control" placeholder="Email">
+
+                <label class="font-weight-bold">Subject </label>
+                <br />
+                <input type="text" name="email" class="form-control" placeholder="Write a reason for contacting us">
+
+                <label class="font-weight-bold">Message </label>
+                <br />
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" placeholder="Write a Message..."></textarea>
+                <div id="button-container">
+                    <div><button type="button" class="btn btn-success mx-auto mt-3">Send Now</button></div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $(".twitter-link").hover(function(){
+                $(".twitter-link").css("color", "yellow");
+                }
+            });
+        });
+    </script>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,224 @@
+@extends('layouts.master')
+
+@section('title')
+    bfox.gg
+@endsection
+
+@section('page_css')
+    <style>
+        #title-container{
+            font-family: 'Alfa Slab One', cursive;
+        }
+
+        #banner{
+            height: 60vh;
+        }
+
+        .carousel-inner > .carousel-item > img {
+            object-fit:cover;
+            height: 60vh !important;
+            width: 350px;
+        }
+
+        #news-feed .row .col-sm {
+            position: relative;
+            padding: 0;
+        }
+
+        #news-feed .row .col-sm p {
+            position: absolute;
+            top: 9rem;
+            left: 0;
+            background-color: black;
+            width: 100%;
+            padding-top: 0.5rem;
+            padding-bottom: 4rem;
+            opacity: 0.5;
+        }
+
+        #news-feed .row .col-sm h6 {
+            color: white;
+            position: absolute;
+            top: 9.5rem;
+            z-index: 1;
+        }
+
+        #news-feed .row .col-sm small {
+            position: absolute;
+            right: 1.5rem;
+            bottom: 0.5rem;
+            z-index: 1;
+        }
+
+        #news-feed .row .col-sm small a {
+            color: #E51937;
+            text-decoration: none;
+        }
+
+        #news-feed .row #nf-1 {
+            background-size: 100%;
+            overflow: hidden;
+        }
+
+        #news-feed .row #nf-1 .inner-div {
+            height: 210px;
+            width: 100%;
+            background-image: url('/images/Sample_news_1.jpg');
+            background-size: 100%;
+            background-position: center;
+            transition: all 1s ease;
+        }
+
+        #news-feed .row #nf-1 .inner-div:hover {
+            transform: scale(1.2);
+        }
+
+        #news-feed .row #nf-2 {
+            background-size: 100%;
+            overflow: hidden;
+        }
+
+        #news-feed .row #nf-2 .inner-div {
+            height: 210px;
+            width: 100%;
+            background-image: url('/images/Sample_news_2.png');
+            background-size: 100%;
+            background-position: center;
+            transition: all 1s ease;
+        }
+
+        #news-feed .row #nf-2 .inner-div:hover {
+            transform: scale(1.2);
+        }
+
+        #news-feed .row #nf-3 {
+            background-size: 100%;
+            overflow: hidden;
+        }
+
+        #news-feed .row #nf-3 .inner-div {
+            height: 210px;
+            width: 100%;
+            background-image: url('/images/Sample_news_3.jpg');
+            background-size: 100%;
+            background-position: center;
+            transition: all 1s ease;
+        }
+
+        #news-feed .row #nf-3 .inner-div:hover {
+        transform: scale(1.2);
+        }
+
+    </style>
+@endsection
+
+@section('nav')
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="#" class="navbar-brand"><img src="images/sample_fox_icon.png" id="logo">BudFox</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="/" class="nav-link active"><u>Home</u></a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Matches</a></li>
+                <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+@endsection
+
+@section('content')
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" data-interval="2000">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" id="banner">
+            <div class="carousel-item active">
+                <img src="images/pexels-borja-lopez-1346154.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/pexels-lucie-liz-3165335.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/pexels-soumil-kumar-735911.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="container mt-5" id="news-feed">
+        <div class="row">
+            <div class="col-sm">
+                <h1 id="title-container">NEWS</h1>
+            </div>
+        </div>
+
+        <div class="row mt-2">
+
+            <div class="col-sm ml-1 mr-1" id="nf-1">
+                <div class="inner-div">
+                                
+                </div>
+                <p>
+                    <h6 class="pl-2"> Sample Team Lorem ipsum dolor sit amet, consectetur</h6>
+                    <small><a href="#">Read more >>> </a></small>
+                </p>
+            </div>
+
+            <div class="col-sm ml-1 mr-1" id="nf-2">
+                <div class="inner-div">
+                                
+                </div>
+                <p>
+                    <h6 class="pl-2"> Sample Team Lorem ipsum dolor sit amet, consectetur</h6>
+                    <small><a href="#">Read more >>> </a></small>
+                </p>
+            </div>
+
+            <div class="col-sm ml-1 mr-1" id="nf-3">
+                <div class="inner-div">
+                                
+                </div>
+                <p>
+                    <h6 class="pl-2"> Sample Team Lorem ipsum dolor sit amet, consectetur</h6>
+                    <small><a href="#">Read more >>> </a></small>
+                </p>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-sm text-center">
+                <button type="button" class="btn btn-outline-danger mx-auto"><h3>View More</h3></button>    		
+            </div>
+        </div>
+    </div>
+
+    <hr />
+    
+@endsection
